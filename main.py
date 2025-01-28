4abad86a28ee11b4d49b30ccdcfa019e518fcfaa
sandronadiradze <nadiradzesandro98@gmail.com>
2025-01-29 01:46:31 +0400

    chore: - Register weather fetching exception handler - Add get_weather endpoint

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 import time
 from contextlib import asynccontextmanager
+from typing import Annotated
 
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, Query, Request
 
 from src.config.logger import logger
+from src.middlewares.exception_handlers import weather_fetch_error_handler
+from src.services.weather_service import WeatherService
+from src.utils.exceptions import WeatherFetchError
 
 
 @asynccontextmanager
@@ -15,7 +19,13 @@ async def lifespan(app: FastAPI):
     logger.info("FastAPI application shutting down")
 
 
-app = FastAPI(lifespan=lifespan)
+app = FastAPI(
+    title="Weather API",
+    description="API service for retrieving weather information for cities",
+    version="1.0.0",
+    lifespan=lifespan,
+)
+app.add_exception_handler(WeatherFetchError, weather_fetch_error_handler)
 
 
 @app.middleware("http")
@@ -40,3 +50,33 @@ async def log_requests(request: Request, call_next):
 def read_root():
     logger.info("Processing root endpoint request")
     return {"message": "Hello, World!"}
+
+
+@app.get("/weather")
+async def get_weather(
+    city: Annotated[
+        str,
+        Query(
+            min_length=2,
+            max_length=50,
+            pattern=r"^[A-Za-zÀ-ÿ\s\-]+$",
+            description="Name of the city to fetch weather data for (supports accented characters)",
+            examples=["London", "New York", "São Paulo", "Tbilisi"],
+        ),
+    ],
+):
+    """
+    Fetch current weather data for a specified city.
+
+    param:
+        city: Name of the city (letters and spaces only (supports accented characters))
+
+    Returns:
+        Dict containing weather data including temperature, humidity,
+        wind speed, and description
+    """
+
+    logger.info(f"Processing weather request for city: {city}")
+    weather_data = await WeatherService.fetch_weather_data(city)
+
+    return weather_data
