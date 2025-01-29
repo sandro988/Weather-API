970cbfdb10bad626360980d925175b0b44f1e9f7
sandronadiradze <nadiradzesandro98@gmail.com>
2025-01-29 18:59:17 +0400

    Add exceptions for dynamoDB service

--- src/utils/exceptions.py
+++ src/utils/exceptions.py
@@ -81,3 +81,57 @@ class CacheError(StorageError):
         super().__init__(
             message=message, status_code=500, original_error=original_error
         )
+
+
+class DynamoDBError(Exception):
+    """Base exception for DynamoDB-related errors."""
+
+    def __init__(
+        self,
+        message: str,
+        status_code: int = 500,
+        original_error: Optional[Exception] = None,
+    ):
+        self.message = message
+        self.status_code = status_code
+        self.original_error = original_error
+        super().__init__(self.message)
+
+
+class DynamoDBConnectionError(DynamoDBError):
+    """Exception raised when unable to connect to DynamoDB."""
+
+    def __init__(
+        self,
+        message: str = "Unable to connect to DynamoDB",
+        original_error: Optional[Exception] = None,
+    ):
+        super().__init__(
+            message=message, status_code=503, original_error=original_error
+        )
+
+
+class DynamoDBDataError(DynamoDBError):
+    """Exception raised for issues with data format or validation."""
+
+    def __init__(
+        self,
+        message: str = "Invalid data format for DynamoDB",
+        original_error: Optional[Exception] = None,
+    ):
+        super().__init__(
+            message=message, status_code=400, original_error=original_error
+        )
+
+
+class DynamoDBPermissionError(DynamoDBError):
+    """Exception raised for permission-related issues with DynamoDB."""
+
+    def __init__(
+        self,
+        message: str = "Permission denied for DynamoDB operation",
+        original_error: Optional[Exception] = None,
+    ):
+        super().__init__(
+            message=message, status_code=403, original_error=original_error
+        )
